11eff3e6c52365711d946f4e12863d67ac99eacb
ksjhiu <94771016+ksjhiu@users.noreply.github.com>
2021-12-26 16:19:14 +0530

    Add files via upload

---
+++ array-myCap.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[5];
+	cout<<"Enter elements:"<<endl;
+	for(int i=0;i<5;i++)
+	{
+		cin >> a[i];
+		
+	}
+	cout<<"You entered:";
+	for(int i=0;i<5;i++)
+	{
+		cout<<a[i]<<endl;
+	}
+	return 0;
+}
+
